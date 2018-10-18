a82222dcb1f955670581bf5bf7557c7a6e8153f3
Newgen <newgen@ebex.esselgroup.local>
2018-10-18 19:09:18 +0530

    18-10-2018 - 1

--- src/app/components/reactive-forms/reactive-forms.component.ts
+++ src/app/components/reactive-forms/reactive-forms.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-reactive-forms',
@@ -7,9 +8,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReactiveFormsComponent implements OnInit {
 
+  // instance of FormControl
+  // setting the initial valeue to '' -> empty string
+  //  you get immediate access to listen for, update, and validate the state of the form input.
+  nameControl = new FormControl('');
+
+  // FormGroup Example
+  profileForm = new FormGroup({
+    firstName: new FormControl(''),
+    lastName: new FormControl(''),
+  });
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  changeName() {
+    this.nameControl.setValue('Aditya');
+  }
+
+  onSubmit() {
+    // TODO: Use EventEmitter with form value
+    console.warn(this.profileForm.value);
   }
 
 }
