4a435e417549bbd0fb30c88353f7e62ea82999b6
Aditya Tyagi <tyagi.aditya844747@gmail.com>
2018-11-15 18:24:08 +0530

    Fetch data from server, display data for particular branch Completed.

--- src/app/components/personalise-nh/personalise-nh.component.ts
+++ src/app/components/personalise-nh/personalise-nh.component.ts
@@ -1,17 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
-export interface NHData {
-  univeristy: {
-    colleges: string[],
-    branches: string[],
-    semesters: string[],
+export interface Universities {
+    _id: string;
+    name: string;
+    colleges: string[];
+    branches: string[];
+    semesters: string[];
     subjects: [
       {
         branch: string[]
       }
-    ]
-  };
+    ];
 }
 
 @Component({
@@ -24,49 +25,86 @@ export class PersonaliseNhComponent implements OnInit {
   // ----------- Class Properties -----------
 
   nhDataUrl = 'http://localhost:4200/assets/data.json';
-  newData: NHData;
-
-  // show/hide data
-  showdata = false;
-  showdata2 = false;
-  dataStatus = 'Show Data';
-  dataStatus2 = 'Show Data';
+  universityData: Universities[] = [];
 
   // filling form selects
-  universities: ['GGSIPU', 'RTU'];
-  colleges: string[];
-  branches: string[];
-  semesters: string[];
+  uNames: any = [];
+  cNames: any = [];
+  bNames: any = [];
+  sNames: any = [];
+  subNames: any = [];
 
-  populateData() {
-    this.colleges = this.newData.univeristy.colleges;
-    this.branches = this.newData.univeristy.branches;
-    this.semesters = this.newData.univeristy.semesters;
+
+  // initialise the form
+  personalForm = this.fb.group({
+    universityName: [''],
+    collegeName: [''],
+    branchName: [''],
+    semesterName: [''],
+    subjectName: ['']
+  });
+
+  // getters for the values of form fields
+  get universityName() {
+    return this.personalForm.get('universityName').value;
   }
 
-  // ----------- Class Methods -----------
+  get collegeName() {
+    return this.personalForm.get('collegeName').value;
+  }
 
+  get branchName() {
+    return this.personalForm.get('branchName').value;
+  }
+
+  get semesterName() {
+    return this.personalForm.get('semesterName').value;
+  }
+
+  get subjectName() {
+    return this.personalForm.get('subjectName').value;
+  }
+
+  // ------------------------------------------ Class Methods -----------------------------------------------
   getData2() {
     // get an observable of NHData
-    return this.http.get<NHData>(this.nhDataUrl);
+    return this.http.get<Universities[]>(this.nhDataUrl);
   }
 
   fetchData() {
-    this.getData2().subscribe((data: NHData) => this.newData = { ...data });
+    this.getData2().subscribe((data: Universities[]) => {
+      this.universityData = data;
+      console.log(this.universityData['universities']);
+    });
   }
 
-  // show/hide of nh data in a cleaner format
-  showData2() {
-    console.log(this.newData);
-    this.showdata2 = !this.showdata2;
-    if (this.showdata2) {
-      this.dataStatus2 = 'Hide Data';
-    } else {
-      this.dataStatus2 = 'Show Data';
+  // populating data from the server data
+  populateData() {
+    alert('Populating Data...');
+    // fetch the university names from the array and push it into univeristies
+    for (let i = 0; i < this.universityData['universities'].length; i++) {
+      this.uNames.push(this.universityData['universities'][i].name);
+    }
+
+    this.bNames = this.universityData['universities'][0].branches;
+    this.cNames = this.universityData['universities'][0].colleges;
+    this.sNames = this.universityData['universities'][0].semesters;
+
+    // fetch branch name
+    const subjectKey =  Object.keys(this.universityData['universities'][0].subjects[0])[0];
+    if (subjectKey === 'IT') {
+      alert('Fetch IT Subjects');
+      this.subNames = this.universityData['universities'][0].subjects[0][subjectKey];
     }
   }
+
+  // submit personal form
+  onSubmitPersonalForm() {
+    alert('Changes accepted.');
+    console.log('Form Values: ', this.personalForm.value);
+  }
   // ----------- Constructor AND ngOnInit -----------
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient, private fb: FormBuilder) { }
 
   ngOnInit() {
     // fetch nh data
